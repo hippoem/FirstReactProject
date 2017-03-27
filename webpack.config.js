0c536fefa27e5e8e30be4a6a8998fb1e1cae6235
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-27 15:08:23 +0700

    Hello World !!
    Set up Reactjs Project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+var config = {
+    entry: './main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+
+module.exports = config;
